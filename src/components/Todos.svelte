bda3d2e00e24c71c764d9338d99e7acba346aae2
Jason Johnson <jasonjohnson47@gmail.com>
2021-10-19 21:01:09 +0000

    getting there...

--- src/components/Todos.svelte
+++ src/components/Todos.svelte
@@ -1,80 +1,78 @@
 <script>
+    import NewTodo from './NewTodo.svelte';
+    import FilterButton from './FilterButton.svelte';
+    import Todo from './Todo.svelte';
+    import MoreActions from './MoreActions.svelte';
+
     export let todos = [];
 
     $: totalTodos = todos.length;
     $: completedTodos = todos.filter((todo) => todo.completed).length;
 
+    let newTodoName = '';
+    let newTodoId;
+    $: {
+      if (totalTodos === 0) {
+        newTodoId = 1;
+      } else {
+        newTodoId = Math.max(...todos.map((t) => t.id)) + 1;
+      }
+    }
+
+    function addTodo(name) {
+      todos = [...todos, { id: newTodoId, name: name, completed: false }];
+      newTodoName = '';
+    }
+
     function removeTodo(todo) {
         todos = todos.filter((t) => t.id !== todo.id);
     }
+
+    function updateTodo(todo) {
+      const i = todos.findIndex( (t) => t.id === todo.id );
+      todos[i] = {...todos[i], ...todo};
+    }
+
+    //$: console.log('newTodoName: ', newTodoName)
+
+    let filter = 'all';
+
+    const filterTodos = (filter, todos) => {
+      if (filter === 'active') {
+        return todos.filter(t => !t.completed);
+      } else if (filter === 'completed') {
+        return todos.filter(t => t.completed);
+      } else {
+        return todos; // all
+      }
+    }
+
+    const checkAllTodos = (completed) => { 
+      todos = todos.map( (t) => {
+        return {...t, completed: completed};
+      });
+    };
+    
+    const removeCompletedTodos = () => todos = todos.filter( (t) => !t.completed );
 </script>
 
 <!-- Todos.svelte -->
 <div class="todoapp stack-large">
 
-  <!-- NewTodo -->
-  <form>
-    <h2 class="label-wrapper">
-      <label for="todo-0" class="label__lg">
-        What needs to be done?
-      </label>
-    </h2>
-    <input type="text" id="todo-0" autocomplete="off"
-      class="input input__lg" />
-    <button type="submit" disabled="" class="btn btn__primary btn__lg">
-      Add
-    </button>
-  </form>
-
-  <!-- Filter -->
-  <div class="filters btn-group stack-exception">
-    <button class="btn toggle-btn" aria-pressed="true">
-      <span class="visually-hidden">Show</span>
-      <span>All</span>
-      <span class="visually-hidden">tasks</span>
-    </button>
-    <button class="btn toggle-btn" aria-pressed="false">
-      <span class="visually-hidden">Show</span>
-      <span>Active</span>
-      <span class="visually-hidden">tasks</span>
-    </button>
-    <button class="btn toggle-btn" aria-pressed="false">
-      <span class="visually-hidden">Show</span>
-      <span>Completed</span>
-      <span class="visually-hidden">tasks</span>
-    </button>
-  </div>
+  <NewTodo on:addTodo={ (e) => addTodo(e.detail) } />
+  <FilterButton bind:filter /><!-- shorthand for bind:filter={filter} -->
 
   <!-- TodosStatus -->
   <h2 id="list-heading">{completedTodos} out of {totalTodos} items completed</h2>
 
   <!-- Todos -->
   <ul role="list" class="todo-list stack-large" aria-labelledby="list-heading">
-    {#each todos as todo (todo.id)}
+    {#each filterTodos(filter, todos) as todo (todo.id)}
         <li class="todo">
-            <div class="stack-small">
-                <div class="c-cb">
-                    <input
-                        type="checkbox"
-                        id="todo-{todo.id}"
-                        on:click={() => todo.completed = !todo.completed}
-                        checked={todo.completed}
-                    >
-                    <label for="todo-{todo.id}" class="todo-label">{todo.name}</label>
-                </div>
-                <div class="btn-group">
-                    <button type="button" class="btn">
-                        Edit <span class="visually-hidden">{todo.name}</span>
-                    </button>
-                    <button
-                        type="button"
-                        class="btn btn__danger"
-                        on:click={() => removeTodo(todo)}
-                    >
-                        Delete <span class="visually-hidden">{todo.name}</span>
-                    </button>
-                </div>
-            </div>
+            <Todo {todo}
+              on:update={ (e) => updateTodo(e.detail) }
+              on:remove={ (e) => removeTodo(e.detail) }
+            />
         </li>
     {:else}
         <li>Nothing to do here!</li>
@@ -83,10 +81,8 @@
 
   <hr />
 
-  <!-- MoreActions -->
-  <div class="btn-group">
-    <button type="button" class="btn btn__primary">Check all</button>
-    <button type="button" class="btn btn__primary">Remove completed</button>
-  </div>
-
+  <MoreActions {todos} 
+    on:checkAll={ (e) => checkAllTodos(e.detail) }
+    on:removeCompleted={removeCompletedTodos}
+  />
 </div>
